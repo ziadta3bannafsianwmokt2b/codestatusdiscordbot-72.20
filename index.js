afda26d6a6cb0282c0c43c5c0a3eb883e7232cb8
ziad amr <ziad.amr97534@gmail.com>
2025-02-25 02:46:55 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+  client.on('ready', () => {
+    let serverCount = client.guilds.cache.size;
+    client.user.setStatus('dnd') // idle online dnd invisible
+            console.log(`🟢 | Ready! Logged in as ${client.user.tag} , My ID : ${client.user.id}`);
+         let activities = [ `Dev : 72.20!`, `${client.guilds.cache.size} servers im in`, `WICK STUDIO` ], i = 0;  // ممكن تحط اكتر من دول 
+        setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Watching }),  4000);//تقدر تغيرها Playing - Streaming - Listening
+    })
