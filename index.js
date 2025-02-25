be9554102dc913fb691f824978d8a9152b26364d
ziad amr <ziad.amr97534@gmail.com>
2025-02-25 18:00:25 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
    const {client , ActivityType} = require ('discord.js');
-   const {joinVoiceChannel} = require ('@discordjs/voice');
-   client.on('messageCreate', (message) => {
-    if (message.mentions.has(client.user) && message.member.voice.channel) {
-      joinVoiceChannel({
-        channelId: message.member.voice.channel.id,
-        guildId: message.guild.id,
-        adapterCreator: message.guild.voiceAdapterCreator,
-      });
-      message.reply(`دخلت الروم بنجاح!`);
-    }
-  });
     client.on('ready', () => {
     let serverCount = client.guilds.cache.size;
     client.user.setStatus('dnd') // idle online dnd invisible
