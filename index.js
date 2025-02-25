716b483368e982faabe4cef7ea9a28e5cc91b3a1
ziad amr <ziad.amr97534@gmail.com>
2025-02-25 20:31:47 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,18 @@
          let activities = [ `Dev : 72.20!`, `${client.guilds.cache.size} servers im in`, `WICK STUDIO` ], i = 0;  // ممكن تحط اكتر من دول 
         setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Watching }),  4000);//تقدر تغيرها Playing - Streaming - Listening
     })
+console.log(`
+                     .___         ___.                  _________ ________        ________  _______   
+  _____  _____     __| _/  ____   \_ |__   ___.__.  /\  \______  \\_____  \       \_____  \ \   _  \  
+ /     \ \__  \   / __ | _/ __ \   | __ \ <   |  |  \/      /    / /  ____/        /  ____/ /  /_\  \ 
+|  Y Y  \ / __ \_/ /_/ | \  ___/   | \_\ \ \___  |  /\     /    / /       \       /       \ \  \_/   \
+|__|_|  /(____  /\____ |  \___  >  |___  / / ____|  \/    /____/  \_______ \  /\  \_______ \ \_____  /
+      \/      \/      \/      \/       \/  \/                             \/  \/          \/       \/ 
+`);
+console.log('your bot is ready!');
+console.log('code by 72.20');
+console.log (`Discord id >> 877717735801487360`)
+console.log(`progarmming >>  discord.gg/witon
+services >> discord.gg/wick-s
+community >> discord.gg/wick-c
+wicks support >> discord.gg/kcz9VEBHp5`)
